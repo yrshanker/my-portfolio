90ee487102259a05a32d1c0833c69e2e1587a9ea
yrshanker <yshanker@andrew.cmu.edu>
2025-02-25 14:12:13 -0500

    Fixed autonomous-vehicle project issue

---
+++ app/projects/autonomous-vehicle/page.tsx
@@ -0,0 +1,9 @@
+export default function AutonomousVehicleProject() {
+    return (
+      <div>
+        <h1>Autonomous Vehicle Project</h1>
+        <p>Details about the project...</p>
+      </div>
+    );
+  }
+  
